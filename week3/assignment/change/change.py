f7e489fb6e20a4e81ce36600c9b6dd0527603cf5
Diego Dell'Era <ddellera@socialmetrix.com>
2016-10-30 16:50:03 -0300

    change coins: done

--- week3/assignment/change/change.py
+++ week3/assignment/change/change.py
@@ -1,10 +1,33 @@
-# Uses python3
+#!/usr/bin/python3
 import sys
 
+debug = False
+
+denominations = [10, 5, 1]
+
+def solve(n_coins, amount):
+    if amount == 0:
+        return n_coins
+    for denomination in denominations:
+        if amount - denomination >= 0:
+            amount -= denomination
+            n_coins += 1
+            if debug: print("denomination", denomination, "n_coins", n_coins, "amount", amount)
+            break
+    return solve(n_coins, amount)
+
 def get_change(m):
-    #write your code here
-    return m
+    return solve(n_coins=0, amount=m)
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
-    print(get_change(m))
+
+    if debug:
+        for (amount, expected) in [(2, 2), (28, 6), (40, 4), (16, 3), (17, 4)]:
+            print("calculating change for amount", amount)
+            result = get_change(amount)
+            print("coins: %d" % expected, "got: %d" % result)
+            print("-"*80)
+            assert(expected == result)
+    else:
+        m = int(sys.stdin.read())
+        print(get_change(m))

42601807d6e98e603e6eeb98a0c8f870b55d2ad6
Diego Dell'Era <ddellera@socialmetrix.com>
2016-10-30 22:33:17 -0300

    dot_product: done

--- week3/assignment/dot_product/dot_product.py
+++ week3/assignment/dot_product/dot_product.py
@@ -1,19 +1,41 @@
-#Uses python3
+#!/usr/bin/python3
 
 import sys
 
-def max_dot_product(a, b):
-    #write your code here
-    res = 0
-    for i in range(len(a)):
-        res += a[i] * b[i]
-    return res
+debug = False
+
+def max_dot_product(profit_per_click, average_clicks):
+    result = 0
+    while profit_per_click != []:
+        best_profit_per_click = max(profit_per_click)
+        best_average_clicks = max(average_clicks)
+        profit_per_click.remove(best_profit_per_click)
+        average_clicks.remove(best_average_clicks)
+        result += best_profit_per_click * best_average_clicks
+        if debug: print("best_profit_per_click", best_profit_per_click, "best_average_clicks", best_average_clicks, "-> result", result)
+    return result
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n = data[0]
-    a = data[1:(n + 1)]
-    b = data[(n + 1):]
-    print(max_dot_product(a, b))
-    
+
+    if debug:
+        a = [23]
+        b = [39]
+        expected = 897
+        result = max_dot_product(a, b)
+        print("-"*80)
+        assert(expected == result)
+
+        a = [1, 3, -5]
+        b = [-2, 4, 1]
+        expected = 23
+        result = max_dot_product(a, b)
+        print("-"*80)
+        assert(expected == result)
+
+    else:
+        input = sys.stdin.read()
+        data = list(map(int, input.split()))
+        n = data[0]
+        a = data[1:(n + 1)]
+        b = data[(n + 1):]
+        print(max_dot_product(a, b))

67cee2e4d275ccc39afd3a4b9975e1bb903a5b1c
Diego Dell'Era <ddellera@socialmetrix.com>
2016-11-01 20:32:18 -0300

    largest number: done

--- week3/assignment/largest_number/largest_number.py
+++ week3/assignment/largest_number/largest_number.py
@@ -1,17 +1,40 @@
-#Uses python3
-
+#!/usr/bin/python3
 import sys
 
-def largest_number(a):
-    #write your code here
-    res = ""
-    for x in a:
-        res += x
-    return res
+debug = False
+
+def is_greater_or_equal(n, m):
+    if not m:
+        return True
+    return int(str(n)+str(m)) >= int(str(m)+str(n))
+
+def largest_number(numbers):
+    result = ""
+    while numbers != []:
+        max_number = None
+        for number in numbers:
+            if debug: print("number", number)
+            if is_greater_or_equal(number, max_number):
+                if debug: print("number", number, "is >=", max_number)
+                max_number = number
+        result += str(max_number)
+        numbers.remove(max_number)
+    return result
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = input.split()
-    a = data[1:]
-    print(largest_number(a))
-    
+
+    if debug:
+        def check(numbers, expected):
+            result = largest_number(numbers)
+            print(numbers, expected, result)
+            assert(expected == result)
+
+        check([2,21], "221")
+        check([9,4,6,1,9], "99641")
+        check([23,39,92], "923923")
+
+    else:
+        input = sys.stdin.read()
+        data = input.split()
+        a = data[1:]
+        print(largest_number(a))

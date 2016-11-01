fc8d09d1b68abbde8e4ff6eb808869a00f47b2b1
Diego Dell'Era <ddellera@socialmetrix.com>
2016-11-01 16:20:42 -0300

    different summands done

--- week3/assignment/different_summands/different_summands.py
+++ week3/assignment/different_summands/different_summands.py
@@ -4,53 +4,18 @@ import sys
 debug = False
 
 def optimal_summands(n):
-    if n == 1:
-        return [1]
-    if n == 2:
-        return [2]
-    return solve(n, 1)
-
-def solve(k, l):
-    if debug: print("solving k", k, "l", l)
-    if k <= 2 * l:
-        summands = list(range(1, l))
-        if k != 0:
-            summands.append(k)
-            if debug: print("added k", k, "summands", summands)
-        return summands
-    else:
-        if debug: print("added l", l)
-        return solve(k-l, l+1)
-
-
-# def optimal_summands(n):
-#     if n == 1:
-#         return [1]
-#     return solve(n, 1, summands = [])
-
-# def solve(k, l, summands):
-#     if k <= 2 * l:
-#         summands.append(k)
-#         if debug: print("added k", k, "summands", summands)
-#         return summands
-#     else:
-#         summands.append(l)
-#         if debug: print("added l", l, "summands", summands)
-#         return solve(k-l, l+1, summands)
-
-# def optimal_summands(n):
-#     summands = []
-#     for i in range(1, n):
-#         n -= i
-#         if n < i:
-#             summands.append(n)
-#             break
-#         elif n == 0:
-#             summands.append(i)
-#             break
-#         else:
-#             summands.append(i)
-#     return summands
+    if n <= 2: return [n]
+    summands = []
+    k = n
+    l = 1
+    while l <= n/2:
+        if k <= 2 * l: break
+        summands.append(l)
+        k -= l
+        l += 1
+    if k != 0:
+        summands.append(k)
+    return summands
 
 if __name__ == '__main__':
 
@@ -60,12 +25,19 @@ if __name__ == '__main__':
             print(n, result)
             assert(sum(result) == n)
 
+        assert([1] == optimal_summands(1))
         assert([2] == optimal_summands(2))
+        assert([1,2] == optimal_summands(3))
+        assert([1,3] == optimal_summands(4))
+        assert([1,4] == optimal_summands(5))
         assert([1,2,3] == optimal_summands(6))
         assert([1,2,5] == optimal_summands(8))
         assert([1,2,3,4,5] == optimal_summands(15))
         assert([1,2,3,4,5,8] == optimal_summands(23))
-            
+        assert(len(optimal_summands(987654321)) == 44443)
+        assert(len(optimal_summands(12345678910)) == 157134)
+        assert(len(optimal_summands(2673516735757)) == 2312364)
+
     else:
         input = sys.stdin.read()
         n = int(input)
@@ -73,3 +45,39 @@ if __name__ == '__main__':
         print(len(summands))
         for x in summands:
             print(x, end=' ')
+
+# --------------------------------------------------------------------------------
+
+# recursive versions:
+
+# def optimal_summands(n):
+#     if n == 1:
+#         return [1]
+#     if n == 2:
+#         return [2]
+#     return solve(n, 1)
+# def solve(k, l):
+#     if debug: print("solving k", k, "l", l)
+#     if k <= 2 * l:
+#         summands = list(range(1, l))
+#         if k != 0:
+#             summands.append(k)
+#             if debug: print("added k", k, "summands", summands)
+#         return summands
+#     else:
+#         if debug: print("added l", l)
+#         return solve(k-l, l+1)
+
+# def optimal_summands(n):
+#     if n == 1:
+#         return [1]
+#     return solve(n, 1, summands = [])
+# def solve(k, l, summands):
+#     if k <= 2 * l:
+#         summands.append(k)
+#         if debug: print("added k", k, "summands", summands)
+#         return summands
+#     else:
+#         summands.append(l)
+#         if debug: print("added l", l, "summands", summands)
+#         return solve(k-l, l+1, summands)

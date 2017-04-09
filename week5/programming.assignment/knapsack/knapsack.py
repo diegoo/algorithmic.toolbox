5c8ba17b7ce490466ee12a7652a67940975b4159
Diego Dell'Era <ddellera@socialmetrix.com>
2017-04-09 19:46:49 -0300

    init knapsack without repetitions

--- week5/programming.assignment/knapsack/knapsack.py
+++ week5/programming.assignment/knapsack/knapsack.py
@@ -1,15 +1,25 @@
-# Uses python3
+#!/usr/bin/python3
 import sys
 
+DEBUG=True
+
 def optimal_weight(W, w):
-    # write your code here
     result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
     return result
 
+
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    W, n, *w = list(map(int, input.split()))
-    print(optimal_weight(W, w))
+
+    if DEBUG:
+        test_cases = [
+            (10, 3, [1, 4, 8], 9),
+        ]
+        for (capacity, numer_of_bars, weights, expected_weight) in test_cases:
+            result = optimal_weight(capacity, weights)
+            print("result:", result)
+            assert(result == expected_weight)
+    
+    else:
+        input = sys.stdin.read()
+        W, n, *w = list(map(int, input.split()))
+        print(optimal_weight(W, w))
